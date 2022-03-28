ac9b6a933879f5aa475af255511af843643467af
Jonatandb <jonatandb@gmail.com>
2022-03-28 20:51:46 -0300

    Using nodemon
    Fix to problem with '--exec babel-node': https://es.stackoverflow.com/a/500041/184688

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express()
 app.listen(3000)
 
 app.get('/', (req, res) => {
-  res.send('Server working!')
+  res.send('Server working!!')
 })
 
 console.log('Server listen on port', 3000)
