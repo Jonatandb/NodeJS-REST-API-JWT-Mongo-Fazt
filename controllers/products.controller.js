ce310568b46419f29665a1fd2f72cc3d3ef90947
Jonatandb <jonatandb@gmail.com>
2022-03-29 01:08:16 -0300

    Creating and getting products

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,9 +1,16 @@
-export const createProduct = (req, res) => {
-  res.json({ message: 'createProduct' })
+import Product from '../models/Product'
+
+export const createProduct = async (req, res) => {
+  const { name, category, price, imgURL } = req.body
+  const newProduct = new Product({ name, category, price, imgURL })
+  const savedProduct = await newProduct.save()
+
+  res.status(201).json(savedProduct)
 }
 
-export const getProducts = (req, res) => {
-  res.json({ message: 'getProducts' })
+export const getProducts = async (req, res) => {
+  const products = await Product.find()
+  res.json(products)
 }
 
 export const getProductById = (req, res) => {
