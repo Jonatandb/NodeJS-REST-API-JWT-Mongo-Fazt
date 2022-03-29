0cae3b5a78f381d50356f7a7880a5870995bda04
Jonatandb <jonatandb@gmail.com>
2022-03-29 00:51:50 -0300

    Setting up dotenv to read .env file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+import database from './database.js'
 import app from './app.js'
 
 app.listen(3000)
 
-console.log('Server listen on port', 3000)
+console.log('Server listening at http://localhost:%s', 3000)
