542e1abbd462d515d8a18f6b87a81cc901757ac1
Jonatandb <jonatandb@gmail.com>
2022-03-28 21:43:41 -0300

    Products routes creation

---
+++ controllers/products.controller.js
@@ -0,0 +1,19 @@
+export const createProduct = (req, res) => {
+  res.json({ message: 'createProduct' })
+}
+
+export const getProducts = (req, res) => {
+  res.json({ message: 'getProducts' })
+}
+
+export const getProductById = (req, res) => {
+  res.json({ message: 'getProductById' })
+}
+
+export const updateProductById = (req, res) => {
+  res.json({ message: 'updateProductById' })
+}
+
+export const deleteProductById = (req, res) => {
+  res.json({ message: 'deleteProductById' })
+}
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import morgan from 'morgan'
 import pkg from '../package.json'
+import productsRoutes from '../routes/products.routes'
 
 const app = express()
 
@@ -16,5 +17,7 @@ app.get('/', (req, res) => {
     version: app.get('pkg').version,
   })
 })
+
+app.use('/products', productsRoutes)
 
 export default app
