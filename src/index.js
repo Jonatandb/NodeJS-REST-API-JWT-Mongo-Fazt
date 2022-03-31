6a13a55f87239b79923168f497abbccf2f03e9d2
Jonatandb <jonatandb@gmail.com>
2022-03-30 22:00:06 -0300

    Using process.env.PORT

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ require('dotenv').config()
 import database from './database.js'
 import app from './app.js'
 
-app.listen(3000)
+app.listen(process.env.PORT)
 
-console.log('Server listening at http://localhost:%s', 3000)
+console.log('Server listening at http://localhost:%s', process.env.PORT)
